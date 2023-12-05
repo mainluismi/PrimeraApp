e507ff8eeab09f828963de2c5959a64f692f73ca
mainluismi <luismisc02@gmail.com>
2023-12-05 12:07:53 +0100

    creacion de aplicacion angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PrimeraApp';
+
+  changeTitle(){
+    this.title = 'changed';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './components/header/header.component';
+import { ProductComponent } from './components/product/product.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
